3fd12e1fd0d7da90312fdf99b3b27fe236687415
Sebastião Willian <swillian2009@gmail.com>
2024-12-10 22:59:20 +0000

    :sparkles: feat: Finalização do DataGrid."

--- teste-proficiencia/Form1.Designer.cs
+++ teste-proficiencia/Form1.Designer.cs
@@ -30,14 +30,7 @@
 		{
 			this.labelAptFis = new System.Windows.Forms.Label();
 			this.labelProfEsc = new System.Windows.Forms.Label();
-			this.groupBox_profIdioma = new System.Windows.Forms.GroupBox();
-			this.radioButton_heb = new System.Windows.Forms.RadioButton();
-			this.radioButton_gre = new System.Windows.Forms.RadioButton();
-			this.radioButton_alem = new System.Windows.Forms.RadioButton();
-			this.radioButton_esp = new System.Windows.Forms.RadioButton();
-			this.radioButton_ing = new System.Windows.Forms.RadioButton();
-			this.radioButton_port = new System.Windows.Forms.RadioButton();
-			this.button1 = new System.Windows.Forms.Button();
+			this.Button1 = new System.Windows.Forms.Button();
 			this.dataGridView1 = new System.Windows.Forms.DataGridView();
 			this.nomeAluno = new System.Windows.Forms.DataGridViewTextBoxColumn();
 			this.AptFis = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -53,13 +46,16 @@
 			this.groupBoxIdiomas = new System.Windows.Forms.GroupBox();
 			this.textBoxNomeAluno = new System.Windows.Forms.TextBox();
 			this.labelNomeAluno = new System.Windows.Forms.Label();
-			this.groupBox_profIdioma.SuspendLayout();
+			this.Button2 = new System.Windows.Forms.Button();
+			this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+			this.groupBox_profIdioma = new System.Windows.Forms.GroupBox();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
 			this.groupBoxProf.SuspendLayout();
 			this.groupBoxIdiomas.SuspendLayout();
+			this.groupBox_profIdioma.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// labelAptFis
@@ -82,116 +78,28 @@
 			this.labelProfEsc.TabIndex = 2;
 			this.labelProfEsc.Text = "Proficiência Escrita";
 			// 
-			// groupBox_profIdioma
+			// Button1
 			// 
-			this.groupBox_profIdioma.Controls.Add(this.radioButton_heb);
-			this.groupBox_profIdioma.Controls.Add(this.radioButton_gre);
-			this.groupBox_profIdioma.Controls.Add(this.radioButton_alem);
-			this.groupBox_profIdioma.Controls.Add(this.radioButton_esp);
-			this.groupBox_profIdioma.Controls.Add(this.radioButton_ing);
-			this.groupBox_profIdioma.Controls.Add(this.radioButton_port);
-			this.groupBox_profIdioma.Location = new System.Drawing.Point(5, 58);
-			this.groupBox_profIdioma.Margin = new System.Windows.Forms.Padding(2);
-			this.groupBox_profIdioma.Name = "groupBox_profIdioma";
-			this.groupBox_profIdioma.Padding = new System.Windows.Forms.Padding(2);
-			this.groupBox_profIdioma.Size = new System.Drawing.Size(155, 170);
-			this.groupBox_profIdioma.TabIndex = 5;
-			this.groupBox_profIdioma.TabStop = false;
-			this.groupBox_profIdioma.Text = "Proficiente";
-			// 
-			// radioButton_heb
-			// 
-			this.radioButton_heb.AutoSize = true;
-			this.radioButton_heb.Location = new System.Drawing.Point(9, 140);
-			this.radioButton_heb.Margin = new System.Windows.Forms.Padding(2);
-			this.radioButton_heb.Name = "radioButton_heb";
-			this.radioButton_heb.Size = new System.Drawing.Size(72, 20);
-			this.radioButton_heb.TabIndex = 5;
-			this.radioButton_heb.TabStop = true;
-			this.radioButton_heb.Text = "Hebraico";
-			this.radioButton_heb.UseVisualStyleBackColor = true;
-			// 
-			// radioButton_gre
-			// 
-			this.radioButton_gre.AutoSize = true;
-			this.radioButton_gre.Location = new System.Drawing.Point(9, 92);
-			this.radioButton_gre.Margin = new System.Windows.Forms.Padding(2);
-			this.radioButton_gre.Name = "radioButton_gre";
-			this.radioButton_gre.Size = new System.Drawing.Size(55, 20);
-			this.radioButton_gre.TabIndex = 4;
-			this.radioButton_gre.TabStop = true;
-			this.radioButton_gre.Text = "Grego";
-			this.radioButton_gre.UseVisualStyleBackColor = true;
-			// 
-			// radioButton_alem
-			// 
-			this.radioButton_alem.AutoSize = true;
-			this.radioButton_alem.Location = new System.Drawing.Point(9, 116);
-			this.radioButton_alem.Margin = new System.Windows.Forms.Padding(2);
-			this.radioButton_alem.Name = "radioButton_alem";
-			this.radioButton_alem.Size = new System.Drawing.Size(63, 20);
-			this.radioButton_alem.TabIndex = 3;
-			this.radioButton_alem.TabStop = true;
-			this.radioButton_alem.Text = "Alemão";
-			this.radioButton_alem.UseVisualStyleBackColor = true;
-			// 
-			// radioButton_esp
-			// 
-			this.radioButton_esp.AutoSize = true;
-			this.radioButton_esp.Location = new System.Drawing.Point(9, 68);
-			this.radioButton_esp.Margin = new System.Windows.Forms.Padding(2);
-			this.radioButton_esp.Name = "radioButton_esp";
-			this.radioButton_esp.Size = new System.Drawing.Size(70, 20);
-			this.radioButton_esp.TabIndex = 2;
-			this.radioButton_esp.TabStop = true;
-			this.radioButton_esp.Text = "Espanhol";
-			this.radioButton_esp.UseVisualStyleBackColor = true;
-			// 
-			// radioButton_ing
-			// 
-			this.radioButton_ing.AutoSize = true;
-			this.radioButton_ing.Location = new System.Drawing.Point(9, 44);
-			this.radioButton_ing.Margin = new System.Windows.Forms.Padding(2);
-			this.radioButton_ing.Name = "radioButton_ing";
-			this.radioButton_ing.Size = new System.Drawing.Size(57, 20);
-			this.radioButton_ing.TabIndex = 1;
-			this.radioButton_ing.TabStop = true;
-			this.radioButton_ing.Text = "Inglês";
-			this.radioButton_ing.UseVisualStyleBackColor = true;
-			// 
-			// radioButton_port
-			// 
-			this.radioButton_port.AutoSize = true;
-			this.radioButton_port.Location = new System.Drawing.Point(9, 20);
-			this.radioButton_port.Margin = new System.Windows.Forms.Padding(2);
-			this.radioButton_port.Name = "radioButton_port";
-			this.radioButton_port.Size = new System.Drawing.Size(76, 20);
-			this.radioButton_port.TabIndex = 0;
-			this.radioButton_port.TabStop = true;
-			this.radioButton_port.Text = "Português";
-			this.radioButton_port.UseVisualStyleBackColor = true;
-			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(336, 423);
-			this.button1.Margin = new System.Windows.Forms.Padding(2);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(58, 38);
-			this.button1.TabIndex = 6;
-			this.button1.Text = "Inserir";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click);
+			this.Button1.BackColor = System.Drawing.Color.MediumSeaGreen;
+			this.Button1.Location = new System.Drawing.Point(302, 387);
+			this.Button1.Margin = new System.Windows.Forms.Padding(2);
+			this.Button1.Name = "Button1";
+			this.Button1.Size = new System.Drawing.Size(58, 38);
+			this.Button1.TabIndex = 8;
+			this.Button1.Text = "Inserir";
+			this.Button1.UseVisualStyleBackColor = false;
+			this.Button1.Click += new System.EventHandler(this.Button1_Click);
 			// 
 			// dataGridView1
 			// 
 			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
 			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-			this.nomeAluno,
-			this.AptFis,
-			this.ProfEsc,
-			this.ProfOra,
-			this.Idiomas,
-			this.Aprovado});
+            this.nomeAluno,
+            this.AptFis,
+            this.ProfEsc,
+            this.ProfOra,
+            this.Idiomas,
+            this.Aprovado});
 			this.dataGridView1.Location = new System.Drawing.Point(302, 12);
 			this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
 			this.dataGridView1.Name = "dataGridView1";
@@ -246,16 +154,26 @@
 			// numericUpDown1
 			// 
 			this.numericUpDown1.Location = new System.Drawing.Point(127, 22);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
 			this.numericUpDown1.Name = "numericUpDown1";
 			this.numericUpDown1.Size = new System.Drawing.Size(63, 23);
-			this.numericUpDown1.TabIndex = 9;
+			this.numericUpDown1.TabIndex = 2;
 			// 
 			// numericUpDown2
 			// 
 			this.numericUpDown2.Location = new System.Drawing.Point(127, 55);
+			this.numericUpDown2.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
 			this.numericUpDown2.Name = "numericUpDown2";
 			this.numericUpDown2.Size = new System.Drawing.Size(63, 23);
-			this.numericUpDown2.TabIndex = 10;
+			this.numericUpDown2.TabIndex = 3;
 			// 
 			// labelProfOra
 			// 
@@ -269,9 +187,14 @@
 			// numericUpDown3
 			// 
 			this.numericUpDown3.Location = new System.Drawing.Point(127, 84);
+			this.numericUpDown3.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
 			this.numericUpDown3.Name = "numericUpDown3";
 			this.numericUpDown3.Size = new System.Drawing.Size(63, 23);
-			this.numericUpDown3.TabIndex = 12;
+			this.numericUpDown3.TabIndex = 4;
 			// 
 			// groupBoxProf
 			// 
@@ -281,7 +204,7 @@
 			this.groupBoxProf.Controls.Add(this.numericUpDown1);
 			this.groupBoxProf.Controls.Add(this.labelProfEsc);
 			this.groupBoxProf.Controls.Add(this.labelAptFis);
-			this.groupBoxProf.Location = new System.Drawing.Point(41, 12);
+			this.groupBoxProf.Location = new System.Drawing.Point(41, 84);
 			this.groupBoxProf.Name = "groupBoxProf";
 			this.groupBoxProf.Size = new System.Drawing.Size(227, 127);
 			this.groupBoxProf.TabIndex = 13;
@@ -290,31 +213,68 @@
 			// 
 			// groupBoxIdiomas
 			// 
-			this.groupBoxIdiomas.Controls.Add(this.textBoxNomeAluno);
-			this.groupBoxIdiomas.Controls.Add(this.labelNomeAluno);
 			this.groupBoxIdiomas.Controls.Add(this.groupBox_profIdioma);
-			this.groupBoxIdiomas.Location = new System.Drawing.Point(41, 178);
+			this.groupBoxIdiomas.Location = new System.Drawing.Point(41, 247);
 			this.groupBoxIdiomas.Name = "groupBoxIdiomas";
-			this.groupBoxIdiomas.Size = new System.Drawing.Size(227, 233);
-			this.groupBoxIdiomas.TabIndex = 14;
+			this.groupBoxIdiomas.Size = new System.Drawing.Size(227, 178);
+			this.groupBoxIdiomas.TabIndex = 5;
 			this.groupBoxIdiomas.TabStop = false;
 			this.groupBoxIdiomas.Text = "Idiomas";
 			// 
 			// textBoxNomeAluno
 			// 
-			this.textBoxNomeAluno.Location = new System.Drawing.Point(108, 30);
+			this.textBoxNomeAluno.Location = new System.Drawing.Point(144, 39);
 			this.textBoxNomeAluno.Name = "textBoxNomeAluno";
-			this.textBoxNomeAluno.Size = new System.Drawing.Size(100, 23);
+			this.textBoxNomeAluno.Size = new System.Drawing.Size(124, 23);
 			this.textBoxNomeAluno.TabIndex = 1;
 			// 
 			// labelNomeAluno
 			// 
 			this.labelNomeAluno.AutoSize = true;
-			this.labelNomeAluno.Location = new System.Drawing.Point(6, 33);
+			this.labelNomeAluno.Location = new System.Drawing.Point(47, 42);
 			this.labelNomeAluno.Name = "labelNomeAluno";
 			this.labelNomeAluno.Size = new System.Drawing.Size(91, 16);
 			this.labelNomeAluno.TabIndex = 0;
 			this.labelNomeAluno.Text = "Nome do aluno:";
+			// 
+			// Button2
+			// 
+			this.Button2.BackColor = System.Drawing.Color.IndianRed;
+			this.Button2.Location = new System.Drawing.Point(364, 387);
+			this.Button2.Margin = new System.Windows.Forms.Padding(2);
+			this.Button2.Name = "Button2";
+			this.Button2.Size = new System.Drawing.Size(58, 38);
+			this.Button2.TabIndex = 9;
+			this.Button2.Text = "Limpar";
+			this.Button2.UseVisualStyleBackColor = false;
+			this.Button2.Click += new System.EventHandler(this.Button2_Click);
+			// 
+			// checkedListBox1
+			// 
+			this.checkedListBox1.FormattingEnabled = true;
+			this.checkedListBox1.Items.AddRange(new object[] {
+            "Alemão",
+            "Espanhol",
+            "Grego",
+            "Hebraico",
+            "Inglês",
+            "Português"});
+			this.checkedListBox1.Location = new System.Drawing.Point(5, 21);
+			this.checkedListBox1.Name = "checkedListBox1";
+			this.checkedListBox1.Size = new System.Drawing.Size(120, 112);
+			this.checkedListBox1.TabIndex = 7;
+			// 
+			// groupBox_profIdioma
+			// 
+			this.groupBox_profIdioma.Controls.Add(this.checkedListBox1);
+			this.groupBox_profIdioma.Location = new System.Drawing.Point(9, 21);
+			this.groupBox_profIdioma.Margin = new System.Windows.Forms.Padding(2);
+			this.groupBox_profIdioma.Name = "groupBox_profIdioma";
+			this.groupBox_profIdioma.Padding = new System.Windows.Forms.Padding(2);
+			this.groupBox_profIdioma.Size = new System.Drawing.Size(155, 147);
+			this.groupBox_profIdioma.TabIndex = 6;
+			this.groupBox_profIdioma.TabStop = false;
+			this.groupBox_profIdioma.Text = "Proficiente";
 			// 
 			// Form1
 			// 
@@ -322,16 +282,17 @@
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.SystemColors.ButtonFace;
 			this.ClientSize = new System.Drawing.Size(1213, 486);
+			this.Controls.Add(this.textBoxNomeAluno);
+			this.Controls.Add(this.Button2);
+			this.Controls.Add(this.labelNomeAluno);
 			this.Controls.Add(this.groupBoxIdiomas);
 			this.Controls.Add(this.groupBoxProf);
 			this.Controls.Add(this.dataGridView1);
-			this.Controls.Add(this.button1);
+			this.Controls.Add(this.Button1);
 			this.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.Margin = new System.Windows.Forms.Padding(2);
 			this.Name = "Form1";
 			this.Text = "Página Principal - Teste Proficência";
-			this.groupBox_profIdioma.ResumeLayout(false);
-			this.groupBox_profIdioma.PerformLayout();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
@@ -339,8 +300,9 @@
 			this.groupBoxProf.ResumeLayout(false);
 			this.groupBoxProf.PerformLayout();
 			this.groupBoxIdiomas.ResumeLayout(false);
-			this.groupBoxIdiomas.PerformLayout();
+			this.groupBox_profIdioma.ResumeLayout(false);
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -348,11 +310,7 @@
 
 		private System.Windows.Forms.Label labelAptFis;
 		private System.Windows.Forms.Label labelProfEsc;
-		private System.Windows.Forms.GroupBox groupBox_profIdioma;
-		private System.Windows.Forms.RadioButton radioButton_esp;
-		private System.Windows.Forms.RadioButton radioButton_ing;
-		private System.Windows.Forms.RadioButton radioButton_port;
-		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button Button1;
 		private System.Windows.Forms.DataGridView dataGridView1;
 		private System.Windows.Forms.NumericUpDown numericUpDown1;
 		private System.Windows.Forms.NumericUpDown numericUpDown2;
@@ -362,15 +320,15 @@
 		private System.Windows.Forms.GroupBox groupBoxIdiomas;
 		private System.Windows.Forms.TextBox textBoxNomeAluno;
 		private System.Windows.Forms.Label labelNomeAluno;
-		private System.Windows.Forms.RadioButton radioButton_heb;
-		private System.Windows.Forms.RadioButton radioButton_gre;
-		private System.Windows.Forms.RadioButton radioButton_alem;
 		private System.Windows.Forms.DataGridViewTextBoxColumn nomeAluno;
 		private System.Windows.Forms.DataGridViewTextBoxColumn AptFis;
 		private System.Windows.Forms.DataGridViewTextBoxColumn ProfEsc;
 		private System.Windows.Forms.DataGridViewTextBoxColumn ProfOra;
 		private System.Windows.Forms.DataGridViewTextBoxColumn Idiomas;
 		private System.Windows.Forms.DataGridViewTextBoxColumn Aprovado;
+		private System.Windows.Forms.GroupBox groupBox_profIdioma;
+		private System.Windows.Forms.CheckedListBox checkedListBox1;
+		private System.Windows.Forms.Button Button2;
 	}
 }
 
--- teste-proficiencia/Form1.cs
+++ teste-proficiencia/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -9,20 +10,85 @@ namespace testeproficiencia
 		public Form1()
 		{
 			InitializeComponent();
+			LimparDadosInseridos();
+			textBoxNomeAluno.Focus();
 		}
 
-		private void button1_Click(object sender, EventArgs e)
+		private void Button1_Click(object sender, EventArgs e)
 		{
-			// Obter valores dos controles
-			string nome = textBoxNomeAluno.Text;
-			int aptFisica = (int)numericUpDown1.Value;
-			int profEscrita = (int)numericUpDown2.Value;
-			int profOral = (int)numericUpDown3.Value;
-			string idioma = groupBox_profIdioma.Controls.OfType<RadioButton>()
-								.FirstOrDefault(r => r.Checked)?.Text ?? "Nenhum";
+			if (textBoxNomeAluno.Text != "")
+			{
+				// Obter valores dos controles
+				string nome = textBoxNomeAluno.Text;
+				int aptFisica = (int)numericUpDown1.Value;
+				int profEscrita = (int)numericUpDown2.Value;
+				int profOral = (int)numericUpDown3.Value;
+				// string idioma = comboBoxIdioma.Text; 
 
-			// Adicionar uma nova linha no DataGridView
-			dataGridView1.Rows.Add(nome, aptFisica, profEscrita, profOral, idioma, "Sim/Não");
+				// Adicionar uma nova linha no DataGridView.
+				dataGridView1.Rows.Add(nome, aptFisica, profEscrita, profOral, /*idioma,*/ VericarAptidao(aptFisica, profEscrita, profOral));
+
+				LimparDadosInseridos();
+			}
+			else
+				MessageBox.Show("Preencha os dados, por favor!");
 		}
+
+		#region Verificações
+		/// <summary>
+		/// Verificará a média das três matérias, junto com o resultado da proficiência dos idiomas. 
+		/// </summary>
+		/// <param name="aptFis">Valor da aptidão física.</param>
+		/// <param name="profEsc">Valor da proficiência escrita.</param>
+		/// <param name="profOral">Valor da proficiência oral.</param>
+		/// <returns>Aprovação ou Reprovação do estudante.</returns>
+		private string VericarAptidao(int aptFis, int profEsc, int profOral)
+		{
+			string resultado = "Reprovado!";
+			if (VerificarProficienciaIdioma())
+			{
+				double media = (aptFis + profEsc + profOral) / 3.0;
+
+				// Obter uma média arredondada em duas casas decimal. 
+				double mediaArredo = Math.Round(media, 2);
+				if (mediaArredo >= 10)
+					resultado = "Aprovado!";
+			}
+				
+
+			return resultado;
+		}
+
+		private bool VerificarProficienciaIdioma()
+		{
+			var linguasSeleccionadas = new List<string>();
+
+			if (checkedListBox1.CheckedItems != null)
+			{
+				foreach (var item in checkedListBox1.CheckedItems)
+					linguasSeleccionadas.Add(item.ToString());
+			}
+			
+			return linguasSeleccionadas.Count() >= 2;
+		}
+		#endregion
+		#region Limpeza
+		private void LimparDadosInseridos()
+		{
+			textBoxNomeAluno.ResetText();
+			numericUpDown1.ResetText();
+			numericUpDown2.ResetText();
+			numericUpDown3.ResetText();
+			
+			for (int i = 0; i < checkedListBox1.Items.Count; i++)
+				checkedListBox1.SetItemChecked(i, false);
+        }
+
+		private void Button2_Click(object sender, EventArgs e)
+		{
+			// Limpar o DataGrid.
+			dataGridView1.Rows.Clear();
+		}
+		#endregion
 	}
 }
